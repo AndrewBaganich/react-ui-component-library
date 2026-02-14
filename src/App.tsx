063f588f1fe24402f8351f31276b3be86608c1f8
AndrewBaganich <andrewbaganich@gmail.com>
2026-02-14 03:38:52 +0200

    implement Toast component with variants and auto-dismiss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import './App.css'
 import { Input } from './components/Input/Input'
 import { useState } from 'react'
+import Toast from './components/Toast/Toast'
 
 function App() {
     const [value, setValue] = useState("")
+    const [showToast, setShowToast] = useState(true)
 
     return(
-        <>
-            <Input
-                placeholder="Type something..."
-                type="password"
-                disabled={false}
-                value={value}
-                onChange={setValue}
-                defaultValue=""
-                clearable={false}
-                label=""
-                error=""
-            />
-        </>
+            <div>
+                <button onClick={() => setShowToast(true)}>Show toast</button>
+                {showToast && (
+                    <Toast 
+                        duration={3000}
+                        onClose={()=> setShowToast(false)}
+                    />
+                )}
+                <Input
+                    placeholder="Type something..."
+                    type="password"
+                    disabled={false}
+                    value={value}
+                    onChange={setValue}
+                    defaultValue=""
+                    clearable={true}
+                    size='sm'
+                />
+            </div>
     )
 }
 
